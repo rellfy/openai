b5c4c4d67ebeb64e6ab8e62b459e465024aea1d5
Valentine <valentinegb@icloud.com>
2022-12-16 14:21:32 -0800

    Add create_completion()

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,10 @@ use reqwest::{
 };
 use serde::Deserialize;
 use models::{ list_models, ModelObject, retrieve_model };
+use completions::{ create_completion, CreateCompletionRequestBody, TextCompletionObject };
 
 pub mod models;
+pub mod completions;
 
 pub(crate) fn openai_headers(openai: &OpenAI) -> HeaderMap {
     let mut headers = HeaderMap::new();
@@ -54,6 +56,8 @@ impl OpenAI<'_> {
             client: Client::new(),
         }
     }
+
+    // MODELS
 
     /// Lists the currently available models, and provides basic information about each one such as the owner and availability.
     ///
@@ -103,5 +107,41 @@ impl OpenAI<'_> {
     /// ```
     pub async fn retrieve_model(&self, model: &str) -> Result<ModelObject, reqwest::Error> {
         retrieve_model(self, model).await
+    }
+
+    // COMPLETIONS
+
+    /// Creates a completion for the provided prompt and parameters
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use openai::{ OpenAI, completions::CreateCompletionRequestBody };
+    /// use dotenv::dotenv;
+    /// use std::env;
+    ///
+    /// #[tokio::main]
+    /// async fn main() {
+    ///     dotenv().expect("should load .env file");
+    ///
+    ///     let key = env::var("OPENAI_KEY").expect("env var OPENAI_KEY should be defined in .env file");
+    ///     let openai = OpenAI::new(&key, None);
+    ///
+    ///     let response = openai.create_completion(CreateCompletionRequestBody {
+    ///         model: "text-davinci-003",
+    ///         prompt: Some("Say this is a test"),
+    ///         max_tokens: Some(7),
+    ///         temperature: Some(0.0),
+    ///         ..CreateCompletionRequestBody::default()
+    ///     }).await.expect("should be text completion");
+    ///
+    ///     assert_eq!(
+    ///         response.choices.first().expect("there should be at least one choice").text,
+    ///         "\n\nThis is indeed a test",
+    ///     );
+    /// }
+    /// ```
+    pub async fn create_completion(&self, body: CreateCompletionRequestBody<'_>) -> Result<TextCompletionObject, reqwest::Error> {
+        create_completion(self, body).await
     }
 }
