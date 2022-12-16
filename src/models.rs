03fc9586c6f205720f77b5d853c306a121190551
Valentine <valentinegb@icloud.com>
2022-12-15 19:28:27 -0800

    Finish ModelObject

--- src/models.rs
+++ src/models.rs
@@ -6,7 +6,27 @@ use super::{ ListObject, OpenAI };
 pub struct ModelObject {
     pub id: String,
     pub object: String,
+    pub created: u32,
     pub owned_by: String,
+    pub permission: Vec<ModelPermissionObject>,
+    pub root: String,
+    pub parent: Option<String>,
+}
+
+#[derive(Deserialize)]
+pub struct ModelPermissionObject {
+    pub id: String,
+    pub object: String,
+    pub created: u32,
+    pub allow_create_engine: bool,
+    pub allow_sampling: bool,
+    pub allow_logprobs: bool,
+    pub allow_search_indices: bool,
+    pub allow_view: bool,
+    pub allow_fine_tuning: bool,
+    pub organization: String,
+    pub group: Option<String>,
+    pub is_blocking: bool,
 }
 
 pub(super) async fn list_models(openai: &OpenAI<'_>) -> Result<ListObject<ModelObject>, reqwest::Error> {
