2d67e26478fe71d2412cabbb664519757c75e261
Valentine <valentinegb@icloud.com>
2022-12-16 11:59:52 -0800

    Document models module

--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,7 @@
+//! List and describe the various models available in the API.
+//! You can refer to the [Models](https://beta.openai.com/docs/models)
+//! documentation to understand what models are available and the differences between them.
+
 use serde::Deserialize;
 use super::{ ListObject, OpenAI, openai_headers };
 
