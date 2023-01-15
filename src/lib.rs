4189e2056189d5abc621e05cecebf9c93be2997d
Valentine <valentinegb@icloud.com>
2023-01-14 22:27:22 -0800

    Change `get_token()` error text

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub(crate) const BASE_URL: &str = "https://api.openai.com/v1";
 
 pub(crate) fn get_token() -> String {
     env::var("OPENAI_KEY")
-        .expect("environment variable `OPENAI_TOKEN` should be defined")
+        .expect("environment variable `OPENAI_KEY` should be defined")
 }
 
 #[derive(Deserialize)]
